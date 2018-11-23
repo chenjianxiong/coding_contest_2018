6046590ffd827cfdbd66842d1cacdb8638973338
j41chen <jianxiong.x.chen@nokia-sbell.com>
2018-11-23 15:31:32 +0800

    Convert to C

--- shuttle_bus.cpp
+++ shuttle_bus.cpp
@@ -1,90 +1,92 @@
 ﻿#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <vector>
+#include <climits>
+#include <stdio.h>
 
-#define DEBUG 0
+static const int MAX_ROW      = 100;
+static const int MAX_COL      = 100;
+static const int LONGEST_DATA = 40;
 
-static const int MAX_ROW = 100;
-static const int MAX_COL = 100;
+static const int EOFOFFILE = INT_MAX;
+static const int SKIPDATA  = INT_MIN;
+
+int getData(FILE* fp, char inputData[]) 
+{
+    int i = 0;
+    int isDigit = 0;
+    char c;
+    while((c = getc(fp)) != EOF) {
+        if(( c >= '0') && (c <= '9')) {
+            isDigit = 1;
+            inputData[i++] = c;
+        }
+        else {
+            if(isDigit) {
+                inputData[i] = '\0';
+                break;
+            }
+        }
+    }
+    if( isDigit ) {
+        return atoi(inputData);
+    }
+    else {
+        if( c == EOF ) {
+            return EOFOFFILE;
+        }
+        else {
+            return SKIPDATA;
+        }
+    }
+}
+
 void handleInputData(char * fileName) 
 {
-    try {        
-        int lastRow[MAX_COL] = {0};
-        int currentRow[MAX_COL] = {0};
-        std::ifstream fs(fileName);
-        if ( fs ) {
-            char c;
-            int data;
-            int M_columnNumber = 0;
-            int N_rowNumber = 0;
-            
-            fs >> M_columnNumber >> c >> N_rowNumber;
-            int col_index = 0;
-            
-            int lastData = 0;
-            int bigData = 0;
-            int newData;
-            int * pTemp = nullptr;
-            int * pCurrentRow = currentRow;
-            int * pLastRow = lastRow;
-            while(fs >> data) {
-                fs.ignore(1, ',');
+    int lastRow[MAX_COL] = {0};
+    int currentRow[MAX_COL] = {0};
+    char longestData[LONGEST_DATA] = {0};
+    FILE * fp = fopen(fileName, "r");        
+    if ( fp ) {
+        int data = 0;
+        int M_columnNumber = 0;
+        int N_rowNumber = 0;
+        int col_index = 0;            
+        int lastData = 0;
+        int bigData = 0;
+        int newData = 0;
+        int * pTemp = nullptr;
+        int * pCurrentRow = currentRow;
+        int * pLastRow = lastRow;
+        M_columnNumber = getData(fp, longestData);
+        N_rowNumber = getData(fp, longestData);
 
-               
-                bigData = ( lastData > pLastRow[col_index] ) ? lastData : pLastRow[col_index];
-                newData = bigData + data;
-                pCurrentRow[col_index] = newData;
-#if DEBUG                  
-                std::cout << "Col:" << col_index << "\tdata:" << data << "\tnew Data:" << newData  << "\t Big data:" << bigData << "\n";
-#endif                
-                if( ++col_index == M_columnNumber) {
-#if DEBUG                    
-                    std::cout << "Last Row:\t" << pLastRow << "\t";
-                    for( int i = 0; i < M_columnNumber; i++)
-                        std::cout << pLastRow[i] << "\t";
-                    std::cout << "\n";
-                    std::cout << "Current Row:\t" << pCurrentRow << "\t";
-                    for( int  i = 0; i < M_columnNumber; i++)
-                        std::cout << pCurrentRow[i] << "\t";
-                    std::cout << "\n";
-#endif                    
-                    col_index = 0;
-                    lastData = 0;
-                    pTemp = pLastRow;
-                    pLastRow = pCurrentRow;
-                    pCurrentRow = pTemp;                    
-                    
-#if DEBUG                    
-                    std::cout << "New Last Row:\t" << pLastRow << "\t";
-                    for( int i = 0; i < M_columnNumber; i++)
-                        std::cout << pLastRow[i] << "\t";
-                    std::cout << "\n";
-#endif                    
-                    
-                }
-                else {
-                    lastData = newData;
-                }
+        while((data = getData(fp, longestData)) != EOFOFFILE) {
+            if (data == SKIPDATA)
+                continue;
+            bigData = ( lastData > pLastRow[col_index] ) ? lastData : pLastRow[col_index];
+            newData = bigData + data;
+            pCurrentRow[col_index] = newData;
+          
+            if( ++col_index == M_columnNumber) {
+                col_index = 0;
+                lastData = 0;
+                pTemp = pLastRow;
+                pLastRow = pCurrentRow;
+                pCurrentRow = pTemp;                    
             }
-            std::cout << newData << std::endl; 
-        }            
+            else {
+                lastData = newData;
+            }
+        }
+        printf("%d\n", newData);
+        fclose(fp);
     }
-    catch (const std::exception& e)
-    {
-        std::cout << "Preocessing data exception." << std::endl;        
-    }
-
-
 }
 
 int main(int argc, char **argv)
 {
     if( argc != 2 ) {
-        std::cout << "Incorrect parameters. Please give your file name." << '\n';
         return -1;
-    }   
-    
+    }    
     handleInputData(argv[1]);
     return 0;
 }
